8f4926534a98b6e44fbaa5d41e2dcf4056767bfe
Mathys Hymon <mathys.hymon@gmail.com>
2024-03-04 17:10:32 +0100

    [feat] weapons

--- Assets/Script/player/playerScript.cs
+++ Assets/Script/player/playerScript.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class playerScript : MonoBehaviour
 {
     [SerializeField] private float shootDelay;
     [SerializeField] private float movementSpeed = 1;
     [SerializeField] private GameObject bulletRef;
+    [SerializeField] private Transform[] bulletSpawn;
 
     private Vector2 input;
     private bool isShooting;
@@ -24,7 +26,7 @@ public class playerScript : MonoBehaviour
 
     public void shoot(InputAction.CallbackContext context)
     {
-        isShooting = context.performed;
+        isShooting = context.ReadValueAsButton();
     }
 
     private void Update()
@@ -32,10 +34,38 @@ public class playerScript : MonoBehaviour
         if(isShooting && delay >= shootDelay)
         {
             delay = 0;
-            Instantiate(bulletRef);
+            for(int i = 0; i < bulletSpawn.Length; i++)
+            {
+                var bullet = Instantiate(bulletRef);
+                bullet.transform.localPosition = bulletSpawn[i].position;
+                bullet.transform.parent = null;
+            }
+        }
+        if(delay < shootDelay)
+        {
+            delay += Time.deltaTime;
         }
 
-        transform.position += new Vector3(input.x * movementSpeed*Time.deltaTime, input.y * movementSpeed*Time.deltaTime, 0);
-        transform.GetChild(0).localRotation =Quaternion.Slerp(transform.GetChild(0).localRotation, Quaternion.Euler(input.x* 10, 90, input.y * 30), 20*Time.deltaTime);
+        if(Mathf.Abs(transform.position.x) >= 11.20f)
+        {
+            transform.position -= new Vector3(transform.position.x/10 * Time.deltaTime, 0,0);
+        }
+
+        else
+        {
+            transform.position += new Vector3(input.x * (movementSpeed / 1.5f) * Time.deltaTime, 0, 0);
+            
+        }
+        if (Mathf.Abs(transform.position.y) >= 6.15f)
+        {
+            transform.position -= new Vector3(0, transform.position.y/10 * Time.deltaTime, 0);
+        }
+        else
+        {
+            transform.position += new Vector3(0, input.y * (movementSpeed / 1.5f) * Time.deltaTime, 0);
+        }
+
+        transform.GetChild(0).localRotation = Quaternion.Slerp(transform.GetChild(0).localRotation, Quaternion.Euler(input.x * 10, 90, input.y * 30), 20 * Time.deltaTime);
+
     }
 }
