800ed2a6bfbf2d6ce7aacc0de9c8bb56ce3a2da2
Mathys Hymon <mathys.hymon@gmail.com>
2024-03-04 15:55:57 +0100

    [fix] player

--- Assets/Script/player/playerScript.cs
+++ Assets/Script/player/playerScript.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class playerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float shootDelay;
+    [SerializeField] private float movementSpeed = 1;
+    [SerializeField] private GameObject bulletRef;
+
+    private Vector2 input;
+    private bool isShooting;
+
+    private float delay;
+    public static playerScript instance;
+
+    private void Start()
     {
-        
+        instance = this;
+    }
+    public void playerMovement(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void shoot(InputAction.CallbackContext context)
     {
-        
+        isShooting = context.performed;
+    }
+
+    private void Update()
+    {
+        if(isShooting && delay >= shootDelay)
+        {
+            delay = 0;
+            Instantiate(bulletRef);
+        }
+
+        transform.position += new Vector3(input.x * movementSpeed*Time.deltaTime, input.y * movementSpeed*Time.deltaTime, 0);
+        transform.GetChild(0).localRotation =Quaternion.Slerp(transform.GetChild(0).localRotation, Quaternion.Euler(input.x* 10, 90, input.y * 30), 20*Time.deltaTime);
     }
 }
